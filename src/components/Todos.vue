c613f8ae65d7f85809d4346846564db8826e0eba
Felipe Butzke <felip.ab@gmail.com>
2021-01-23 19:41:50 -0300

    Criado Todos.vue

---
+++ src/components/Todos.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <H1>Todos</H1>
+</div>
+    
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: "Todos"
+    
+})
+</script>
+<style scoped>
+
+</style>
