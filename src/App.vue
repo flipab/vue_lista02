53511c4e040751c391ffd6b38ef6eb1e7614a11c
Felipe Butzke <felip.ab@gmail.com>
2021-01-23 19:36:58 -0300

    adcionados 03 todos no array

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,28 @@ export default {
   components: {
     
   },//adicionando array de todos
-  data( {
-    return{
-      todos: [
+  data() {
+    return {
+      todos: [ 
+      {
+        id: 1,
+        title: "Todo 01",
+        completed: true
+      },
+       {
+        id: 2,
+        title: "Todo 02",
+        completed: true
+      },
+       {
+        id: 3,
+        title: "Todo 03",
+        completed: true
+      }
 
       ]
     }
-  })
+  }
 }
 </script>
 
