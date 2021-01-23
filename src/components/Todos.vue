c413aa7f2d763bdf622c1d4fb0a1cec9729b328a
Felipe Butzke <felip.ab@gmail.com>
2021-01-23 19:53:24 -0300

    v-bind key e v-for (pesquisar)

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,13 +1,16 @@
 <template>
 <div>
-    <H1>Todos</H1>
+     <div v-bind:key="todo.id" v-for="todo in todos">
+         <h3>{{todo.title}}</h3>
+     </div>
 </div>
     
 </template>
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-    name: "Todos"
+    name: "Todos",
+    props: ["todos"]
     
 })
 </script>
