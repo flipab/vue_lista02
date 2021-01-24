11ab475e0dc64f929a738f2bcec4d07953c0bb38
Felipe Butzke <felip.ab@gmail.com>
2021-01-24 10:52:08 -0300

    Criado o form de "Adicione Tarefas"

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Header />
+    <AddTodo />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
@@ -8,12 +9,15 @@
 <script>
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+
 
 export default {
   name: 'App',
   components: {
     Header,
-    Todos
+    Todos,
+    AddTodo
     
   },//adicionando array de todos
   data() {
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+      <form>
+          <input type="text" placeholder="Adicione sua tarefa aqui">
+          <input type="submit" value="Submit" class="btn">
+      </form>
+  </div>
+</template>
+
+<script >
+
+export default {
+    name: "AddTodo"
+    
+}
+</script>
+
+<style scoped>
+
+form {
+   display: flex;
+   margin-top: 5%;
+   margin-bottom: 5%;
+   border: none;
+}
+
+input[type="text"] {
+    flex: 10;
+    padding: 5px;
+    border: none;
+    background-color: #f4f4f4;
+    padding: 2%;
+
+}
+
+.btn {
+    display: inline-block;
+    border: none;
+    background-color: royalblue;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+}
+
+.btn:hover {
+    background-color: darkblue;
+    color: dodgerblue;
+}
+
+input[type="submit"] {
+    flex: 2;
+}
+
+</style>
