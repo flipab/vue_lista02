3e85d2f21ca6c92dbe36293874c9348f005978f8
Felipe Butzke <felip.ab@gmail.com>
2021-01-24 09:46:41 -0300

    adicionando evento -- caixas de checkbox (v-on:change)

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,6 +1,8 @@
 <template>
 <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
-     <p>{{todo.title}}</p>
+     <p>
+         <input type="checkbox" v-on:change="markComplete">
+         {{todo.title}}</p>
 </div>
     
 </template>
